4b62ae8d3b1e0842888c4dc6b1b630c1b49cc4b3
chanbinchoi <classy9611@gmail.com>
2026-05-26 07:00:45 +0900

    Initial commit

---
+++ src/main/java/com/nagoya/fridge/FridgeHelperApplication.java
@@ -0,0 +1,13 @@
+package com.nagoya.fridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgeHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nagoya/fridge/FridgeHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagoya.fridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
